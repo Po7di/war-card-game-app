842528c3d317d82fb54b855fdd2b465caf2a60b9
Po7di <henszkew@gmail.com>
2023-03-31 10:16:14 +0200

    Initial Commit

---
+++ war-card-game-app/war_card_game_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_card_game_appApp.swift
+//  war-card-game-app
+//
+//  Created by Waldemar Henszke on 31/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct war_card_game_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
