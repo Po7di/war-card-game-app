9e43e4fa15742e1393b65e036e7542ede650f6f2
Po7di <henszkew@gmail.com>
2023-04-05 11:06:28 +0200

    adding assets, learning  how to use containers, functions and structures

--- war-card-game-app/ContentView.swift
+++ war-card-game-app/ContentView.swift
@@ -9,16 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack {
+            
+            Image("background")
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image("card13")
+                    Spacer()
+                    Image("card14")
+                    Spacer()
+                }
+                Spacer()
+                
+                Button(action: {}, label: {
+                    Image("dealbutton")
+                })
+                
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                       
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                        
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
+            
         }
-        .padding()
+        
     }
 }
-
+    
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
